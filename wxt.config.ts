680b4089c2da086b640f42113385ea9639b24db4
Chasan Moustafa <chmoustafa@hotmail.de>
2026-08-30 22:01:01 +0200

    Feat: add extension icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,5 +9,21 @@ export default defineConfig({
       'Adds intentional friction before YouTube videos to interrupt mindless watching.',
     version: '1.0.0',
     permissions: ['storage'],
+    icons: {
+      16: 'icons/icon-16.png',
+      32: 'icons/icon-32.png',
+      64: 'icons/icon-64.png',
+      128: 'icons/icon-128.png',
+      512: 'icons/icon-512.png',
+    },
+    action: {
+      default_title: 'OneLessVideo',
+      default_icon: {
+        16: 'icons/icon-16.png',
+        32: 'icons/icon-32.png',
+        64: 'icons/icon-64.png',
+        128: 'icons/icon-128.png',
+      },
+    },
   },
 });
